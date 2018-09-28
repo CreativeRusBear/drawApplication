6a139c9c24660aa0044b3badd87747afbd2d6582
Artem <37180024+CreativeRusBear@users.noreply.github.com>
2018-09-28 20:53:29 +0300

    Добавлены новые инструменты, а также некоторый функционал

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,13 @@
 window.onload = init;
 var fileOpen =false;
+var hideSection=false;
 function init() {
     var file = document.getElementById("file");
     file.onclick = elementsInFile;
+    var hideAndShowElements=document.getElementById("hideElements");
+    hideAndShowElements.onclick=hideAndShowTools;
+    var linkOnMyGitRepository=document.getElementById("aboutProduct");
+    linkOnMyGitRepository.onclick=repository;
 }
 
 function elementsInFile(eventObj) {
@@ -20,5 +25,21 @@ function elementsInFile(eventObj) {
         block.style.background = "#fbfcfd";
         fileOpen=true;
     }
+};
 
-}
+function hideAndShowTools(eventObj) {
+    var tools=document.getElementById("tools");
+    if (hideSection){
+        hideSection=false;
+        tools.style.display="block";
+        eventObj.target.innerHTML="&#8743;";
+    } else{
+        hideSection=true;
+        tools.style.display="none";
+        eventObj.target.innerHTML="&#8744;";
+    }
+};
+
+function repository() {
+    window.open('https://github.com/CreativeRusBear/drawApplication');
+};
